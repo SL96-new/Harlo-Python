9fd3f0a380ecbb45cd955d709d162590b10c7ecc
SL96-new <marco.lun.324@gmail.com>
2022-03-28 19:43:09 +0800

    exercise 2.3.6

---
+++ exercise 2_3_6.py
@@ -0,0 +1,16 @@
+username = input ()
+hello = "Hello " + username + ", would you like to learn some Python today?"
+print (hello)
+
+# string.method 
+print (username.lower())
+print (username.upper())
+print (username.title())
+
+famous_person = "JK"
+message = famous_person + "said: \"JUST DO IT\" "
+
+name = "\t  Emma Watson\nTom Holland\t.     "
+name = name.lstrip()
+name = name.rstrip()
+print (name)
