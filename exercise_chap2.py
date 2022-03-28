6ff4a36a4cd3cb3d4d8235f65403fe2918d8d637
SL96-new <marco.lun.324@gmail.com>
2022-03-28 23:02:00 +0800

    exercise

--- exercise_chap2.py
+++ exercise_chap2.py
@@ -1,15 +1,18 @@
+# 2-3 username + greeting
 username = input ()
 hello = "Hello " + username + ", would you like to learn some Python today?"
 print (hello)
 
-# string.method 
+# 2-4 string.method 
 print (username.lower())
 print (username.upper())
 print (username.title())
 
+# 2-5 & 6 combine variable and string 
 famous_person = "JK"
 message = famous_person + "said: \"JUST DO IT\" "
 
+# 2-7 try strip method
 name = "\t  Emma Watson\nTom Holland\t.     "
 name = name.lstrip()
 name = name.rstrip()
---
+++ exercise_chap3.py
@@ -0,0 +1,28 @@
+# 3-1 print with index
+names=["jinkun","oonteng","weiann","leeyang","jiazhen"]
+print (names[0])
+print (names[1])
+print (names[2])
+print (names[3])
+print (names[4])
+
+# 3-2 combine name with greeting
+for x in names:
+    print ((x)+" Hi!")
+
+'''
+add item
+1. append : add item to last position of list
+2. insert : add item to given index 
+    e.g. (2,'new item')
+
+delete item
+1. del : delete item by index(postion)
+    e.g. del list[0]
+2. pop(): delete last item in list
+    / or pop (index): delete specified item
+3. remove : delete specified item
+    e.g remove ("specified item")
+
+'''
+
