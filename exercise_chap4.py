cc7afba6a73ad12017ecba5fb3bf367579dfa83a
SL96-new <marco.lun.324@gmail.com>
2022-03-30 22:56:13 +0800

    Chap4 exercise

---
+++ exercise_chap4.py
@@ -0,0 +1,11 @@
+# 4-1 create favourite pizza list
+pizzas = ["Hawaii Chicken","Pepperoni","Spicy Seafood"]
+for pizza in pizzas:
+    print (("I like ")+(pizza) +(" pizza!"))
+print ("I really love pizza")
+
+# 4-3 print 1-20
+for num in range(1,21):
+    print (num)
+
+# 4-4 & 5 sum from 1 to 1 million
