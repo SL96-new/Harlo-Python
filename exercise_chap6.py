5f819818e43b6f29ac3ba494c43ffb5606f6604f
SL96-new <marco.lun.324@gmail.com>
2022-04-03 16:58:16 +0800

    continue exercise chap_6

--- exercise_chap6.py
+++ exercise_chap6.py
@@ -1,7 +1,4 @@
 # 6-1 Person Information
-from multiprocessing.managers import Namespace
-
-
 SL = {"first_name":"SL","last_name":"N","age":"26","city":"KL"}
 for k,v in SL.items():
     print ("\nKey:" + k)
@@ -21,3 +18,27 @@ for name in names:
         print (name + ", Thanks for taking poll!")
     else:
         print (name + ", please pick your answer!")
+
+# 6-7 Nested Dictionary in List
+SL = {"first_name":"Shao Lun","last_name":"Ng","age":"26"}
+JK = {"first_name":"Jin Kun","last_name":"Chai","age":"26"}
+OT = {"first_name":"Oon Teng","last_name":"Wong","age":"28"}
+people = [SL,JK,OT]
+
+for person in people:
+        print ("Full Name:" + person["first_name"] + " " +person["last_name"])
+        print ("Age:" + person["age"])
+
+# 6-10 Nested list in Dictionary [Favourite Num]
+Fav_nums = {
+    "JinKun":["0806","1996"],
+    "LeeYang":["0626","8","1010"],
+    "OonTeng":["0928","0214"],
+    "JiaZhen":"0302",
+    "Weiann":["0731","88"],
+    }
+
+for name,numbers in Fav_nums.items():
+    print (name.title() + " favourite number are:")
+    for number in numbers:
+        print ("\t" + number)
