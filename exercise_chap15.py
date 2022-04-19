ad01ef5a11315101e3f41e63c4427f07ff7a83e1
SL96-new <marco.lun.324@gmail.com>
2022-04-20 00:08:46 +0800

    exercise chap_15

---
+++ exercise_chap15.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x_values = [range (1,5001)]
+y_values = [x**3 for x in x_values]
+
+plt.scatter (x_values, y_values,c = y_values, cmap=plt.cm.Red, edgecolors ='none', s=30)
+
+plt.title("Cubid Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Cubid of Value", fontsize=14)
+
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
