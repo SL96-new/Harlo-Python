83e6df1e7f486106836f9eb50b987621701381a3
SL96-new <marco.lun.324@gmail.com>
2022-04-25 20:59:17 +0800

    update and revise

--- exercise_chap15.py
+++ exercise_chap15.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = list(range (1,11))
 y_values = [x**3 for x in x_values]
 
-plt.scatter (x_values, y_values,c = y_values, cmap=plt.cm.Reds, edgecolors ='none', s=30)
+plt.scatter (x_values, y_values, c=y_values, cmap=plt.cm.Reds, edgecolors ='none', s=30)
 
 plt.title("Cubid Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
