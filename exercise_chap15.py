47815fa6df7d18d463044f4bff6c86d7deb7f758
SL96-new <marco.lun.324@gmail.com>
2022-04-26 22:17:08 +0800

    update exercise chap_15

--- exercise_chap15.py
+++ exercise_chap15.py
@@ -11,3 +11,16 @@ plt.ylabel("Cubid of Value", fontsize=14)
 
 plt.tick_params(axis='both', which='major', labelsize=14)
 plt.show()
+
+import matplotlib.pyplot as plt
+
+x_values = range (1,11)
+y_values = [x**3 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors=None, s=20)
+
+plt.xlabel("Value",fontsize=15)
+plt.ylabel("Cubid of Value",fontsize=15)
+
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
